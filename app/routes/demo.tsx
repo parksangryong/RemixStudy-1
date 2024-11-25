ac5886c6d214f4a796b79c4586c0c0d629eed699
parksangryong <parksangryong@Mac-mini-2.local>
2024-11-25 15:51:46 +0900

    loder1

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -1,3 +1,17 @@
+import { json } from "@remix-run/node";
+
+export async function loader() {
+  const data = { key: "value", date: new Date() };
+  // const body = JSON.stringify(data);
+
+  return json(data, { status: 200 });
+  // return new Response(body, {
+  //   headers: {
+  //     "Content-Type": "application/json; charset=utf-8",
+  //   },
+  // });
+}
+
 export default function Demo() {
   return <h1>Demo page</h1>;
 }
