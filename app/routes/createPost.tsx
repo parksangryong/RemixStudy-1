096c37fde23c2206d87e52610c915f39d7f886f0
parksangryong <parksangryong@Mac-mini-2.local>
2024-12-05 17:37:16 +0900

    유효성 검사

--- app/routes/createPost.tsx
+++ app/routes/createPost.tsx
@@ -1,7 +1,18 @@
 import { ActionFunctionArgs, json } from "@remix-run/node";
-import { useActionData } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import data from "../../postData.json";
 import fs from "fs";
+
+type ActionData = {
+  errors?: {
+    userId?: string;
+    slug?: string;
+    title?: string;
+    content?: string;
+  };
+  success?: boolean;
+};
+
 export async function action({ request }: ActionFunctionArgs) {
   //   console.log("action function called");
   const body = await request.formData();
@@ -9,6 +20,24 @@ export async function action({ request }: ActionFunctionArgs) {
   const slug = body.get("slug");
   const userId = body.get("userId");
   const content = body.get("content");
+
+  const errors: Record<string, string> = {};
+
+  if (!userId || userId == null || userId == "") {
+    errors.userId = "Invalid User ID";
+  }
+  if (!slug || slug == null || slug == "") {
+    errors.slug = "Invalid Slug";
+  }
+  if (!title || title == null || title == "") {
+    errors.title = "Invalid Title";
+  }
+  if (!content || content == null || content == "") {
+    errors.content = "Invalid Content";
+  }
+  if (Object.keys(errors).length > 0) {
+    return json({ errors }, { status: 400 });
+  }
 
   const author = "John Doe";
   const createdAt = formatDate();
@@ -38,32 +67,42 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function CreatePost() {
-  const data = useActionData<typeof action>();
-  console.log(data);
-
+  const actionData = useActionData<ActionData>();
   return (
     <div>
-      <form method="POST">
+      <Form method="POST">
         <section>
           <label htmlFor="userId">User ID:</label>
+          {actionData?.errors?.userId && (
+            <em style={{ color: "red" }}>{actionData?.errors?.userId}</em>
+          )}
           <input type="number" name="userId" />
         </section>
         <section>
           <label htmlFor="slug">Slug:</label>
+          {actionData?.errors?.slug && (
+            <em style={{ color: "red" }}>{actionData?.errors?.slug}</em>
+          )}
           <input type="text" name="slug" />
         </section>
         <section>
           <label htmlFor="title">Title:</label>
+          {actionData?.errors?.title && (
+            <em style={{ color: "red" }}>{actionData?.errors?.title}</em>
+          )}
           <input type="text" name="title" />
         </section>
         <section>
           <label htmlFor="content">Content:</label>
+          {actionData?.errors?.content && (
+            <em style={{ color: "red" }}>{actionData?.errors?.content}</em>
+          )}
           <input type="text" name="content" />
         </section>
         <section>
           <button type="submit">Create Post</button>
         </section>
-      </form>
+      </Form>
     </div>
   );
 }
